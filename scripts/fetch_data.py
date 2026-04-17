157b7b2ef4856eff887faf8bc1e0ac537111ec4c
CodeMindsX-max <codemindsx@gmail.com>
2026-04-17 16:10:45 +0500

    Initial commit

--- scripts/fetch_data.py
+++ scripts/fetch_data.py
@@ -1,11 +1,34 @@
+# import pandas as pd
+
+
+# file_path = "data/raw/Stock Exchange KSE 100(Pakistan).csv"
+# df = pd.read_csv(file_path)
+
+# print(df.head())
+
+# print(df.info())
+
+# print(df.describe())
+
+
+import os
 import pandas as pd
+import requests
 
+RAW_PATH = "data/raw/"
+os.makedirs(RAW_PATH, exist_ok=True)
 
-file_path = "data/raw/Stock Exchange KSE 100(Pakistan).csv"
-df = pd.read_csv(file_path)
+def fetch_psx_data():
+    url = "https://example.com/psx_data.csv"  # replace with real source
 
-print(df.head())
+    response = requests.get(url)
 
-print(df.info())
+    if response.status_code == 200:
+        with open(RAW_PATH + "psx_data.csv", "wb") as f:
+            f.write(response.content)
+        print("Data downloaded successfully!")
+    else:
+        print("Failed to fetch data")
 
-print(df.describe())
+if __name__ == "__main__":
+    fetch_psx_data()
